e4066579612c3500f741e359f252df6237a675e7
Emma PROVOT <Emma.PROVOT@etu.uca.fr>
2020-12-24 18:10:22 +0100

    ajout des images et mises en page

--- projet/migrations/Version20201222175836.php
+++ projet/migrations/Version20201222175836.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201207171216 extends AbstractMigration
+final class Version20201222175836 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20201207171216 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE post DROP category');
+        $this->addSql('ALTER TABLE post ADD filename VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE post ADD category VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE post DROP filename');
     }
 }
